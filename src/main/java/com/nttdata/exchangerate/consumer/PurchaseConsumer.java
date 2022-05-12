58b6e8735e0b06e03552e35b0efa3a1e832a833c
JosueVargasGuia <44180784+JosueVargasGuia@users.noreply.github.com>
2022-05-12 16:46:29 -0500

    precio de bootcoin

--- src/main/java/com/nttdata/exchangerate/consumer/PurchaseConsumer.java
+++ src/main/java/com/nttdata/exchangerate/consumer/PurchaseConsumer.java
@@ -1,11 +1,20 @@
 package com.nttdata.exchangerate.consumer;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+import com.nttdata.exchangerate.entity.ExchangeRate;
+import com.nttdata.exchangerate.service.ExchangeRateService;
+ 
 import com.nttdata.purchaserequest.model.PurchaseRequestKafka;
 
 import lombok.extern.log4j.Log4j2;
@@ -17,9 +26,22 @@ public class PurchaseConsumer {
 	String exchangeTopicResponse;
 	@Autowired
 	KafkaTemplate<String, PurchaseRequestKafka> kafkaTemplate;
+	@Autowired
+	ExchangeRateService exchangeRateService;
 
 	@KafkaListener(topics = "${api.kafka-uri.exchange-topic}", groupId = "group_id")
-	public void purchaseConsumer(PurchaseRequestKafka purchaseRequestKafka) {
-		log.info("Reciviendo purchase para calcular el pricio");
+	public void purchaseConsumer(PurchaseRequestKafka purchaseRequestKafka) throws ParseException {
+		log.info("Reciviendo purchase para calcular el precio");
+		SimpleDateFormat dmy = new SimpleDateFormat("yyyy-MM-dd");
+		SimpleDateFormat dmyNow = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date _dateIni = dmyNow.parse(dmy.format(new Date()) + " 00:00:00");
+		Date _dateEnd = dmyNow.parse(dmy.format(new Date()) + " 11:59:59");
+		ExchangeRate exchangeRate = exchangeRateService.findAll()
+				.filter(e -> e.getCreationDate().getTime() >= _dateIni.getTime()
+						&& e.getCreationDate().getTime() <= _dateEnd.getTime())
+				.collect(Collectors.maxBy(Comparator.comparing(ExchangeRate::getIdExchangeRate))).blockOptional().get()
+				.get();
+		purchaseRequestKafka.setAmountInCurrency(purchaseRequestKafka.getAmountBitcoin()*exchangeRate.getExchangeRateSale());
+		kafkaTemplate.send(exchangeTopicResponse,purchaseRequestKafka);
 	}
 }
